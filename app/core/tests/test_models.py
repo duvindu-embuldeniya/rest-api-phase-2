cd2f751c796bf5e0199fa6a1b59a22b1d6537ac3
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-11-14 20:44:06 +0530

    added superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -41,10 +41,12 @@ class ModelTests(TestCase):
 
 
 
+
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'duvindu', 'sample123')
+
 
 
 
