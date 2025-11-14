ccb6ed9c4c91665bd80a186d745b2d80c9999356
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-11-14 19:58:42 +0530

    added custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,13 +11,15 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
+        name = 'testname'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
+            name=name,
             password=password,
         )
         """exactly equal to the email you passed when creating the user"""
         self.assertEqual(user.email, email)
-        
+
         """password you gave was correctly hashed and saved -> returns True"""
         self.assertTrue(user.check_password(password))
